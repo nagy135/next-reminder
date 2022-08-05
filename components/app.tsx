a08cde695a2460d9952eba74542099bcaf83cb7e
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-08-05 12:38:38 +0200

    feat: app logged in separation

---
+++ components/app.tsx
@@ -0,0 +1,21 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function App() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        APPPPPPP
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
+
