6dbb9f496507796f37e04d6dc10888e51625525f
Viktor Nagy <viktor.nagy@01people.com>
2022-10-12 14:46:11 +0200

    feat: reactQuery + style change

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,31 +3,36 @@ import React from "react";
 import { useMockSession } from "../hooks/session-mock";
 import Login from "./login";
 import styles from "../styles/Home.module.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface ILayout {
   children?: JSX.Element;
 }
 
+const queryClient = new QueryClient();
+
 const Layout: React.FC<ILayout> = ({ children }) => {
   const { data: session } = useMockSession();
   return (
-    <div>
-      <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/daisyui@2.17.0/dist/full.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <script src="https://cdn.tailwindcss.com" defer></script>
-        <title>Reminder</title>
-        <meta
-          name="description"
-          content="Reminds you about stuff you forget!"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>{!session ? <Login /> : children}</main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Head>
+          <link
+            href="https://cdn.jsdelivr.net/npm/daisyui@2.17.0/dist/full.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <script src="https://cdn.tailwindcss.com" defer></script>
+          <title>Reminder</title>
+          <meta
+            name="description"
+            content="Reminds you about stuff you forget!"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={styles.main}>{!session ? <Login /> : children}</main>
+      </div>
+    </QueryClientProvider>
   );
 };
 
--- components/record-list.tsx
+++ components/record-list.tsx
@@ -1,53 +1,46 @@
-import { TGetRecordsResponse } from "@ctypes/response";
-import { useEffect, useState } from "react";
 import GetRecords from "../services/internal/api/get-records";
 import { useMockSession } from "../hooks/session-mock";
 import { dateStringToPrettyDate } from "../utils/common";
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
 
 export default function RecordList() {
   const { data: session } = useMockSession();
   const router = useRouter();
-  const [records, setRecords] = useState<TGetRecordsResponse>([]);
+  const { data: records } = useQuery(["records"], () => {
+    if (session?.user?.email) return GetRecords(session.user.email);
+  });
 
-  useEffect(() => {
-    if (session?.user?.email)
-      GetRecords(session.user.email).then((response) => setRecords(response));
-  }, []);
   return (
+    <>
     <div className="max-w-3xl t-2">
-      <div className="flex justify-center mt-2">
+      {records && records.map((record, i) => (
+        <div
+          tabIndex={i}
+          key={`collapse-item-${i}`}
+          className="collapse border border-orange-400 bg-base-100 rounded-box my-1 pt-2"
+        >
+          <div className="collapse-title text-xl font-medium">
+            <p>
+              <span className="text-orange-500 font-bold">{record.name}</span>
+            </p>
+            <div className="absolute right-2 top-1">{dateStringToPrettyDate(record.deadline)}</div>
+          </div>
+          <div className="collapse-content">
+            <p className="mb-2">{record.description}</p>
+          </div>
+        </div>
+      ))}
+
+    </div>
+      <div className="fixed bottom-0 w-1/2 translate-x-1/4">
         <button
           onClick={() => router.push("/add")}
-          className="btn btn-lg btn-secondary m-auto w-1/2"
+          className="btn btn-lg btn-primary m-auto w-1/2"
         >
           Add record
         </button>
       </div>
-      {records.map((record, i) => (
-        <div
-          tabIndex={i}
-          key={`collapse-item-${i}`}
-          className="collapse border border-gray-600 bg-base-100 rounded-box my-1"
-        >
-          <div className="collapse-title text-xl font-medium">
-            <p>{record.name}</p>
-            <p>{dateStringToPrettyDate(record.deadline)}</p>
-          </div>
-          <div className="collapse-content">
-            <p>{record.description}</p>
-            <p>{record.notified}</p>
-            <p>
-              <input
-                type="checkbox"
-                checked={record.notified}
-                readOnly={true}
-                className="checkbox"
-              />
-            </p>
-          </div>
-        </div>
-      ))}
-    </div>
+      </>
   );
 }
